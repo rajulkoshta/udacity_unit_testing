be7d42a9a82988120b340fd2ad226844a93aed78
“Rajul” <rajulkoshta10@gmail.com>
2021-10-10 12:41:00 +0530

    added files testing

---
+++ test_compute_launch.py
@@ -0,0 +1,21 @@
+from compute_launch import days_until_launch
+
+
+def test_days_until_launch_4():
+    assert (days_until_launch(22, 26) == 4)
+
+
+def test_days_until_launch_0():
+    assert (days_until_launch(253, 253) == 0)
+
+
+def test_days_until_launch_0_negative():
+    assert (days_until_launch(64, 64) == 0)
+
+
+def test_days_until_launch_1():
+    assert (days_until_launch(9, 10) == 1)
+
+
+def test_days_until_launch_3():
+    assert (days_until_launch(50, 100) == 50)
